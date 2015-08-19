0cd36da2c45280581385315eb2dd07129df8b6d1
chadcates <cates.chad@gmail.com>
2015-08-19 16:08:30 -0500

    made comments in artist.js and created profile page for artist

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
+// required variables
+
 var fs = require('fs');
-
 var express = require('express');
-
 var artist = require('./routes/artist');
 var bodyParser = require('body-parser');
-
 var app = express();
+var router = express.Router;
 
 app.set('view engine', 'ejs');
-
 app.locals.title = 'Nodetunes';
 app.use(bodyParser.urlencoded({extended: true}));
 
+//  required routes
+
+// app.use('/album', album);
 app.use('/artist', artist);
+app.use(express.static('www'));
 
 require('./lib/mongodb');
 
+//  nodemon app (starts server)
 
 var port = process.env.PORT || 3000;
-
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%d', host, port);
 });
+
+module.exports = router;
--- routes/artist.js
+++ routes/artist.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var ObjectID = require('mongodb').ObjectID;
 
+//  Get all artist
 
 router.get('/', function (req, res){
 	var collection = global.db.collection('artist');
-
-	collection.find().toArray(function (err, artists) {
+    collection.find().toArray(function (err, artists) {
 		var formattedArtist = artists.map(function (artist) {
 			return {
 				_id:     artist._id,
@@ -18,17 +18,27 @@ router.get('/', function (req, res){
 	});
 });
 
-router.get('/new', function (req, res) {
-	res.render('templates/artistnew');
-});
+
+//  Searches database for artist by name
+
+router.get('/search', function (req, res) {
+      db.collection('artist').find({artist: req.query.artist}).toArray(function (err, artist) {
+      res.render('templates/artistindex', {artists: artist});
+  });
+})
+
+
+//  Saves artist to the database
 
 router.post('/', function (req, res) {
 	var collection = global.db.collection('artist');
-
-	collection.save(req.body, function() {
+    collection.save(req.body, function() {
 		res.redirect('/artist')
    });
 });
+
+
+//  Deletes artist from artist index page and database
 
 router.post('/delete/:id', function (req, res) {
 	var collection = global.db.collection('artist');
@@ -39,8 +49,7 @@ router.post('/delete/:id', function (req, res) {
 
 router.post('/new/:id/submit', function (req, res) {
 	var collection = global.db.collection('artist');
-
-	collection.update(
+    collection.update(
     {_id: ObjectID(req.params.id)},
     {$set: {complete: true}},
     function () {
